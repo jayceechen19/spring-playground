b07a82f2ba9cab4671000eafa7920254341e04fb
jessicachen <C19jessica.chen@usafa.edu>
2020-07-20 15:45:12 -0400

    query params exercise

--- src/main/java/com/example/demo/Paths.java
+++ src/main/java/com/example/demo/Paths.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,35 @@ public class Paths {
     @GetMapping("/")
     public String helloWord(){
         return "Hello from Spring!";
+    }
+
+    @GetMapping("/math/pi")
+    public Double mathPi(){
+        return 3.141592653589793;
+    }
+    @GetMapping("/math/calculate")
+    public String mathCalculate(
+            @RequestParam(value="operation", required=false, defaultValue = "")String operation,
+            @RequestParam(value="x",required=true)Integer x,
+            @RequestParam(value="y", required=true)Integer y
+    )
+    {
+        Integer result;
+        String str;
+        if(operation.equals("multiply")){
+            result = x*y;
+            str = x +" * " +y+" = " +result;
+
+        }else if(operation.equals("divide")){
+            result = x/y;
+            str = x +" / " +y+" = " +result;
+        }else if(operation.equals("subtract")){
+            result = x-y;
+            str = x +" - " +y+" = " +result;
+        }else {
+            result = x+y;
+            str = x +" + " +y+" = " +result;
+        }
+        return str;
     }
 }
